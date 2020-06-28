0aac93b493bb7b869b7206f5ee6c491c5d0c1871
Guilherme Gps <guilherme.santos@eigmercados.com.br>
2020-06-28 12:18:50 -0300

    Efeito para manter menu aberto na tela atual

--- src/main/webapp/js/pages/layout.js
+++ src/main/webapp/js/pages/layout.js
@@ -28,17 +28,18 @@
 	function montarSidebar(lista){
 		let itensMenu = {};
 		$.each(lista, function(index, menu) {
+			let ativo = (!isBlank(menu.url) && menu.url==window.location.pathname) ? true : false;
 			if(menu.codMenuPai!=null){
 				if(isBlank(itensMenu[menu.codMenuPai])){
 					itensMenu[menu.codMenuPai] = $(`
 						<li class="nav-item">
 							<a class="nav-link collapsed" href="#"
-									data-toggle="collapse" data-target="#collapseTwo"
-									aria-expanded="true" aria-controls="collapseTwo"> 
+									data-toggle="collapse" data-target="#collapse-${menu.codMenuPai}"
+									aria-expanded="true" aria-controls="collapse-${menu.codMenuPai}"> 
 								<i class="fas fa-fw ${menu.iconeMenuPai}"></i> 
 								<span>${menu.descMenuPai}</span>
 							</a>
-							<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
+							<div id="collapse-${menu.codMenuPai}" class="collapse" aria-labelledby="heading-${menu.codMenuPai}"
 								data-parent="#accordionSidebar">
 								<div class="bg-white py-2 collapse-inner rounded">
 								</div>
@@ -47,12 +48,17 @@
 					`);
 				}
 				itensMenu[menu.codMenuPai].children('div.collapse').children('div.collapse-inner').append(`
-					<a class="collapse-item" href="${menu.url}">${menu.descricao}</a>
+					<a class="collapse-item ${ativo ? 'active' : ''}" href="${menu.url}">${menu.descricao}</a>
 				`);
+				if(ativo){
+					itensMenu[menu.codMenuPai].addClass('active');
+					itensMenu[menu.codMenuPai].children('a.nav-link').removeClass('collapsed');
+					itensMenu[menu.codMenuPai].children('div.collapse').addClass('show');
+				}
 			} else if(!isBlank(menu.url)){
 				itensMenu[menu.codigo] = $(`
 					<hr class="sidebar-divider">
-					<li class="nav-item active">
+					<li class="nav-item ${ativo ? 'active' : ''}">
 						<a class="nav-link" href="${menu.url}"> 
 							<i class="fas fa-fw ${menu.icone}"></i>
 							<span>${menu.descricao}</span>
